a9899b502c7baf859f8f82a428adb48051be895f
Kyle Rader <kyrader@microsoft.com>
2022-03-24 17:26:32 -0700

    implement C# cli

--- wdp-dotnet/Program.cs
+++ wdp-dotnet/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.CommandLine;
-using System.IO;
+﻿using System.Security.Cryptography;
 
 public enum Action {
     Lock,
@@ -10,7 +9,24 @@ public class Program {
     /// <param name="file">The file to operate on</param>
     /// <param name="action">The action to take</param>
     public static void Main(FileInfo file, Action action) {
-        Console.WriteLine($"file: {file}");
-        Console.WriteLine($"action: {action}");
+        if (!file.Exists) {
+            Console.Error.WriteLine($"{file} does not exist!");
+            Environment.Exit(1);
+        }
+
+        if (action == Action.Lock)
+        {
+            var output_file = $"{file.FullName}.locked";
+            var bytes = File.ReadAllBytes(file.FullName);
+            var data = ProtectedData.Protect(bytes, optionalEntropy: null, scope: DataProtectionScope.CurrentUser);
+            File.WriteAllBytes(output_file, data);
+        }
+        else if (action == Action.Unlock)
+        {
+            var output_file = $"{file.FullName}.unlocked";
+            var bytes = File.ReadAllBytes(file.FullName);
+            bytes = ProtectedData.Unprotect(bytes, null, DataProtectionScope.CurrentUser);
+            File.WriteAllBytes(output_file, bytes);
+        }
     }
 }
