96d6303d5408b9dace5ca28541b290f37fed8dfe
Kyle Rader <kyrader@microsoft.com>
2022-03-24 17:02:35 -0700

    Move rust into rust folder and add dotnet CLI

---
+++ wdp-dotnet/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.CommandLine;
+using System.IO;
+
+public enum Action {
+    Lock,
+    Unlock,
+}
+
+public class Program {
+    /// <param name="file">The file to operate on</param>
+    /// <param name="action">The action to take</param>
+    public static void Main(FileInfo file, Action action) {
+        Console.WriteLine($"file: {file}");
+        Console.WriteLine($"action: {action}");
+    }
+}
